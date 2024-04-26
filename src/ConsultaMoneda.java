787b118c46a39de30a50e4987668a456c6350485
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-04-25 21:41:09 -0600

    Se agregó la petición de la API

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class ConsultaMoneda {
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+    public Moneda buscaMoneda (String monedaBase, String monedaDestino, double cantidad) {
+
+
+        URI uri = URI.create("https://v6.exchangerate-api.com/v6/e2b9379c342d94cb75e1ccf8/pair/" +
+                monedaBase + "/" +
+                monedaDestino + "/" +
+                cantidad);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Moneda.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa moneda.");
+        }
+
+
+    }
 }
+
+
